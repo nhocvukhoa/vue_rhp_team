5792bd2600df6bb6569979698424ce09f7b9e05b
nhocvukhoa <nvanhkhoa148@gmail.com>
2022-12-11 10:26:41 +0700

    split components and study name component

---
+++ chapter_5/section4_good_structure/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <server-details></server-details>
+        </div>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue'
+    import Servers from './components/Servers/Servers.vue'
+    import ServerDetails from './components/Servers/ServerDetails.vue'
+    import Footer from './components/Shared/Footer.vue'
+
+    export default {
+        components: {
+            appHeader: Header, //appHeader sẽ được phiên dịch thành app-header
+            Servers, //Servers sẽ được phiên dịch thành servers
+            'server-details': ServerDetails, //đặt tên component kiểu này cho khỏe
+            'app-footer': Footer
+        }
+    }
+</script>
---
+++ chapter_5/section4_good_structure/src/components/Servers/Servers.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12 col-sm-6">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    Server #
+                </li>
+                <li class="list-group-item">
+                    Server #
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid blue;
+    }
+</style>
