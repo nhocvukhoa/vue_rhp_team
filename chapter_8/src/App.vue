ce659fa7fe78bf7cff372411c253f3aa86f7669d
nhocvukhoa <nvanhkhoa148@gmail.com>
2022-12-22 21:55:33 +0700

    create core component

---
+++ chapter_8/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <app-items-layout :items="items"></app-items-layout>
+    </div>
+</template>
+
+<script>
+    import ItemsLayout from './components/ItemsLayout.vue'
+    export default {
+        data: function() {
+            return {
+                items: [
+                    'Câu hỏi 1',
+                    'Câu hỏi 2'
+                ],
+                maxItems: 10
+            }
+        },
+        components: {
+            appItemsLayout: ItemsLayout
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ chapter_8/src/components/ItemsLayout.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="row">
+        <app-items v-for="item in items" v-bind:data="item" v-bind:key="item">{{ item }}</app-items>
+   </div>
+</template>
+
+<script>
+    import Items from './Items.vue';
+
+    export default {
+        props: ['items'],
+        components: {
+            'appItems': Items
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
