f429a4517f0ff612ef4fd5e78a2edff2819d77a8
nhocvukhoa <nvanhkhoa148@gmail.com>
2022-12-23 17:02:15 +0700

    add new item

--- chapter_8/src/App.vue
+++ chapter_8/src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container">
+        <app-new-item @itemAdded="addItem"></app-new-item>
         <app-items-layout :items="items"></app-items-layout>
     </div>
 </template>
 
 <script>
-    import ItemsLayout from './components/ItemsLayout.vue'
+    import ItemsLayout from './components/ItemsLayout.vue';
+    import NewItem from './components/NewItem.vue';
+
     export default {
         data: function() {
             return {
@@ -17,7 +20,13 @@
             }
         },
         components: {
-            appItemsLayout: ItemsLayout
+            appItemsLayout: ItemsLayout,
+            appNewItem: NewItem
+        },
+        methods: {
+            addItem(item) {
+                this.items.push(item);
+            }
         }
     }
 </script>
--- chapter_8/src/components/NewItem.vue
+++ chapter_8/src/components/NewItem.vue
@@ -1,16 +1,29 @@
 <template>
-   <div class="row">
-        <app-items v-for="item in items" v-bind:data="item" v-bind:key="item">{{ item }}</app-items>
-   </div>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-md-6 form-group">
+                <label for="">Nhập câu hỏi</label>
+                <textarea class="form-group" v-model="item" name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div class="col-sm-8 col-md-6">
+                <button class="btn btn-success" @click.prevent="createNewItem">Add item</button>
+            </div>
+        </form>
+    </div>
 </template>
 
 <script>
-    import Items from './ItemsLayout.vue';
-
     export default {
-        props: ['items'],
-        components: {
-            'appItems': Items
+        data: function() {
+            return {
+                item: ''
+            }
+        },
+        methods: {
+            createNewItem() {
+                this.$emit('itemAdded', this.item);
+                this.item = '';
+            }
         }
     }
 </script>
