210ef342fdd4e72f83ee769146b3ae5f4b2c76dc
nhocvukhoa <nvanhkhoa148@gmail.com>
2022-12-29 21:29:55 +0700

    use data radio button

--- chapter_10/src/App.vue
+++ chapter_10/src/App.vue
@@ -42,10 +42,10 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="male">
-                        <input type="radio" id="male" value="Male"> Male
+                        <input type="radio" id="male" value="Male" v-model="userData.gender"> Male
                     </label>
                     <label for="female">
-                        <input type="radio" id="female" value="Female"> Female
+                        <input type="radio" id="female" value="Female" v-model="userData.gender"> Female
                     </label>
                 </div>
             </div>
@@ -80,7 +80,7 @@
                         <ul>
                             <li v-for="item in sendMail" v-bind:key="item">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
+                        <p>Gender: {{ userData.gender }}</p>
                         <p>Priority:</p>
                         <p>Switched:</p>
                     </div>
@@ -97,7 +97,8 @@
                 userData: {
                     email: '',
                     password: '',
-                    age: 0
+                    age: 0,
+                    gender: 'Male'
                 },
                 message: '',
                 sendMail: [],
