ceab726b28dad90ad974b0687e486b0e8bc15692
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-02 15:40:40 +0700

    add table of content in readme.md and learn about hook function in directive

--- chapter_11/src/App.vue
+++ chapter_11/src/App.vue
@@ -1,5 +1,20 @@
 <template>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Default/Built-in Directives</h1>
+                <p v-text="'Something...'"></p>
+                <p v-html="'<h1>This is heading one</h1>'"></p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Color style by directives</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
--- chapter_11/src/main.js
+++ chapter_11/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+    bind(el, binding, vnode) {
+        el.style.backgroundColor = 'yellow';
+    },
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
