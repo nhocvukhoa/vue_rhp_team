ae33f2978e6499bf5c390a3283e3cce3b8bd3dcb
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-03 18:33:17 +0700

    use global and local simple filter

--- chapter_12/src/App.vue
+++ chapter_12/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filter and Mixins</h1>
-                <p>{{ text | tolowerCase }}</p>
+                <p>{{ text | toUperCase | toLowerCase }}</p>
             </div>
         </div>
     </div>
@@ -17,7 +17,7 @@
             }
         },
         filters: {
-            tolowerCase(text) {
+            toLowerCase(text) {
                 return text.toLowerCase();
             }
         }
--- chapter_12/src/main.js
+++ chapter_12/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('toUperCase', function(value) {
+    return value.toUpperCase();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
