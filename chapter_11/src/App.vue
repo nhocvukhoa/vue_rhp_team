976056524a62e631d0bf5a9ee5892b03b903b43f
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-03 09:30:22 +0700

    use directive global modifiers

--- chapter_11/src/App.vue
+++ chapter_11/src/App.vue
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight:background="'red'">Color style by directives</p>
+                <p v-highlight:background.delayed="'red'">Color style by directives</p>
             </div>
         </div>
     </div>
--- chapter_11/src/main.js
+++ chapter_11/src/main.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//Đây là 1 directive ở dạng global
 Vue.directive('highlight', {
     //el: element node
     //binding: truy cập vào giá trị của directive
     bind(el, binding, vnode) {
-        if (binding.arg == 'background') {
-            el.style.backgroundColor = binding.value;
-        } else {
-            el.style.color = binding.value;
-        }
+        var delay = 0;
+
+        if (binding.modifiers['delayed']) {
+            delay = 3000;
+
+            setTimeout(() => {
+                if (binding.arg == 'background') {
+                    el.style.backgroundColor = binding.value;
+                } else {
+                    el.style.color = binding.value;
+                }
+            }, delay);
+        }   
     },
 });
 
