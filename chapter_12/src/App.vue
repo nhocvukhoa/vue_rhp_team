8ac764a5417f2b236aa5b3f7861a7e9797d6aae6
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-03 21:29:10 +0700

    filter and computed

--- chapter_12/src/App.vue
+++ chapter_12/src/App.vue
@@ -6,6 +6,19 @@
                 <p>{{ text | toUperCase | toLowerCase }}</p>
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h3>Filter products</h3>
+                <input type="text" class="form-control mb-md-3" v-model="filterProduct">
+                <div class="card">
+                    <div class="card-body">
+                        <ul>
+                            <li v-for="product in filteredProduct" v-bind:key="product">{{ product }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,12 +26,21 @@
     export default {
         data() {
             return {
-                text: 'Hello everyone, Vue Js'
+                text: 'Hello everyone, Vue Js',
+                products: ['Iphone', 'Samsung', 'Nokia', 'HTC'],
+                filterProduct: '',
             }
         },
         filters: {
             toLowerCase(text) {
                 return text.toLowerCase();
+            }
+        },
+        computed: {
+            filteredProduct() {
+                return this.products.filter((element) => {
+                    return element.match(this.filterProduct);
+                });
             }
         }
     }
