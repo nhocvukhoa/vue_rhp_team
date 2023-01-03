385de5a7057d2bdc7b3e47ba92522793047e3a73
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-03 15:24:31 +0700

    study filter and mixins

---
+++ chapter_12/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filter and Mixins</h1>
+                <p>{{ text | tolowerCase }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'Hello everyone, Vue Js'
+            }
+        },
+        filters: {
+            tolowerCase(text) {
+                return text.toLowerCase();
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
