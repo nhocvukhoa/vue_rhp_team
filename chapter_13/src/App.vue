5f882b20de0a012c1e4c9fafd94f8a97118646dc
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-05 10:44:12 +0700

    study transition and animation

---
+++ chapter_13/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Filter and Mixins</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
