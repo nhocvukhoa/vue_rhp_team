ee94398261d19acff2b8849c84727b68481616bc
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-11 22:17:23 +0700

    vue resource and firebase

---
+++ chapter_14/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-left">
+                <h1 class="text-center">Vue resource</h1>
+                <hr>
+                <div class="form-group">
+                    <input class="form-control" type="text" v-model="user.userName">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-block btn-success" type="submit" @click="submit">Submit</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+                return {
+                    user: {
+                        userName: '',
+                        email: ''
+                    }
+                }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style scope>
+   
+</style>
