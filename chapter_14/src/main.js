eaeb62cda46fad5586dfbda2357d4d28d1388e1f
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-12 18:34:05 +0700

    interceptor with request

--- chapter_14/src/main.js
+++ chapter_14/src/main.js
@@ -6,6 +6,16 @@ Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vue-rhp-form-ff662-default-rtdb.firebaseio.com/data.json';
 
+Vue.http.interceptors.push((request, next) => {
+    console.log(request);
+
+    if (request.method == 'POST') {
+        request.method = 'PUT';
+    }
+
+    next();
+});
+
 new Vue({
   el: '#app',
   render: h => h(App),
