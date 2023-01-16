494f746dd5dba13914ab332a10795c67a3196bd8
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-16 11:13:31 +0700

    study vue routing

---
+++ chapter_15/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-8 col-sm-offset-2 col-md-6 col-md-offset-3 text-left">
+                <h1 class="text-center">Vue routing</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style scope>
+   
+</style>
