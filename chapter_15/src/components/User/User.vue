c50a47d0e31b767cf30c566745cff1f7729c037e
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-01-17 15:37:40 +0700

    use button to redirect page

--- chapter_15/src/components/User/User.vue
+++ chapter_15/src/components/User/User.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <h1>User page</h1>
+        <hr>
+        <button class="btn btn-success" @click="backHome">Go to home</button>
     </div>
 </template>
 
-<script>
-
+<script> 
+    export default {
+        methods: {
+            backHome() {
+                this.$router.push('/');
+            }
+        }
+    };
 </script>
 
 <style>
